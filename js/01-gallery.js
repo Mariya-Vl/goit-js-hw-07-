837ff2fcdd41817e075c70482220ffe346fecb01
Mariia <mariya.s.vlasova@gmail.com>
2023-07-12 20:01:44 +0300

    Initial commit

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+// створюємо розмітку галереї
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}
+            width="300"
+            />
+        </a>
+       </li>`;
+    })
+    .join("");
+}
+// додаємо розмітку галереї до DOM дерева:
+
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+// додаємо прослуховувача на галерею та відкриваємо модальне вікно при клікі на картинку
+gallery.addEventListener("click", clickOnImg);
+
+let instance = "";
+
+// функція для відкриття модалки:
+function clickOnImg(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+  instance = basicLightbox.create(`
+<div>
+<img src="${evt.target.dataset.source}" width="800" height="600">
+/div>
+`);
+  instance.show();
+}
+
+document.addEventListener("keydown", pressEsc);
+
+// функція для закриття модалки:
+function pressEsc(evt) {
+  const visible = basicLightbox.visible();
+  if (evt.code === "Escape" && visible) {
+    instance.close();
+    gallery.removeEventListener("click", clickOnImg);
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,36 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log("galleryItems:",galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+// створюємо розмітку галереї
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+        alt="${description}"
+        />
+      </a>
+    </li>`;
+    })
+    .join("");
+};
+
+// додаю розмітку до html сторінки:
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+// додаємо прослуховувача подій на клік по картці:
+gallery.addEventListener("click", clickOnImg());
+
+// функція відкриття модалки:
+function clickOnImg(){
+    let lightbox = new SimpleLightbox('.gallery a',{captionsData: `alt`, captionDelay: 250, });
+    };
